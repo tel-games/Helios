0a19e15f0e07181b42385973616933a595ed2fc3
TommanCZ <tomas.partl.jr@email.cz>
2021-10-06 19:34:08 +0200

    rozvrh prikaz

---
+++ src/main/java/cz/helios/commands/School.java
@@ -0,0 +1,49 @@
+/*
+package cz.helios.commands;
+
+import cz.helios.commands.handle.CommandsListener;
+import cz.helios.config.Config;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class School extends CommandsListener {
+    @Override
+    public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+        String[] příkaz = event.getMessage().getContentRaw().split(" ");
+
+       String prefix = ";school";
+       String prefix1 = ";rozvrh";
+
+        if (příkaz[0] == prefix)
+        {
+            if (příkaz[1] == prefix1)
+            {
+                switch (příkaz[2])
+                {
+                    case "gjk":
+                        event.getGuild().getTextChannelById().sendMessage("< 08:30 [Fyz] 09:15 >< 09:25 [HSt] 10:10 >< 10:20 [ M ] 10:05 >< 09:25 [Zem] 10:10 >< 09:20 [---] 10:05 >< 09:15 [Čj ] 10:00 >< 09:10 [Děj] 10:55 >< 09:00 [Děj] 10:45 >").queue();
+                        event.getGuild().getTextChannelById().sendMessage("< 08:30 [Čj ] 09:15 >< 09:25 [ M ] 10:10 >< 10:20 [Aj ] 10:05 >< 09:25 [Inf] 10:10 >< 09:20 [Inf] 10:05 >< 09:15 [Nj ] 10:00 >< 09:10 [Lab] 10:55 >< 09:00 [Lab] 10:45 >").queue();
+                        event.getGuild().getTextChannelById().sendMessage("< 08:30 [Tv ] 09:15 >< 09:25 [Tv ] 10:10 >< 10:20 [Aj ] 10:05 >< 09:25 [Ev ] 10:10 >< 09:20 [Ev ] 10:05 >< 09:15 [Nj ] 10:00 >< 09:10 [Ch ] 10:55 >").queue();
+                        event.getGuild().getTextChannelById().sendMessage("< 08:30 [Bio] 09:15 >< 09:25 [Ch ] 10:10 >< 10:20 [Zem] 10:05 >< 09:25 [ M ] 10:10 >< 09:20 [---] 10:05 >< 09:15 [H/M] 10:00 >").queue();
+                        event.getGuild().getTextChannelById().sendMessage("< 08:30 [Čj ] 09:15 >< 09:25 [Tv ] 10:10 >< 10:20 [Aj ] 10:05 >< 09:25 [Nj ] 10:10 >< 09:20 [Fyz] 10:05 >< 09:15 [-/U] 10:00 >< 09:10 [ÚDJ] 10:55 >").queue();
+                        break;
+                    case "gkb":
+                        event.getGuild().getTextChannelById().sendMessage("Sorry. Data not found.").queue();
+                        break;
+                    case "ssps":
+                        event.getGuild().getTextChannelById().sendMessage("Error 404").queue();
+                        break;
+                    default:
+                        event.getGuild().getTextChannelById().sendMessage("School not found").queue();
+                        break;
+
+
+                }
+            }
+
+
+        }
+    }
+}
+
+ */
